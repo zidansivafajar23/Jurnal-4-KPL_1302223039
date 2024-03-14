0c344bd8fd6b6e1dda65819ec780671ddd1b7205
zidansivafajar23 <zidansivafajar23@gmail.com>
2024-03-14 10:30:31 +0700

    menambahkan class KodeBuah

---
+++ modul4_1302223039/KodeBuah.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302223039
+{
+    public class KodeBuah
+    {
+        public enum buah
+        {
+             Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung 
+        }
+        
+
+        public static string getKodeBuah(buah namaBuah) {
+            string[] kode = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00" };
+
+
+            return kode[(int)namaBuah];
+
+        }
+    }
+}
